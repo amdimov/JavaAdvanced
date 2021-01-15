0490f40b01f1c1f84a74d83782b0e17a5b91507d
Alex Dimov <amdimov@gmail.com>
2021-01-15 15:37:28 +0200

    New Exercise from Lab

--- StackAndQueues/src/MatchingBrackets.java
+++ StackAndQueues/src/MatchingBrackets.java
@@ -1,8 +1,23 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class MatchingBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String expression = scanner.nextLine();
+        ArrayDeque<Integer> resultStack = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == '('){
+                resultStack.push(i);
+
+            }else if(currentChar == ')'){
+                System.out.println(expression.substring(resultStack.pop(), i+1));
+            }
+
+        }
     }
 }
